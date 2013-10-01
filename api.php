c56dfe0f29c67f714c27bdccee02a9450aa58862
Nathaniel van Diepen <eeems@omnimaga.org>
2013-10-01 14:49:53 -0600

    Adding framework

--- api.php
+++ api.php
@@ -1,4 +1,33 @@
 <?php
 	// MYSQL default bugs:bugs
 	// TODO - Add API handling.
+	$method = $_SERVER['REQUEST_METHOD'];
+	if(isset($_GET['type'])){
+		if(isset($_GET['id']){
+			$id = $_GET['id'];
+			switch($_GET['type']){
+				case 'user':
+					// TODO - handle user requests
+				break;
+				case 'group':
+					// TODO - handle group requests
+				break;
+				case 'issue':
+					// TODO - handle issue requests
+				break;
+				case 'scrum':
+					// TODO - handle scrum requests
+				break;
+				case 'admin':
+					// TODO - handle admin requests
+				break;
+				default:
+					// TODO - handle type invalid
+			}
+		}else{
+			// TODO - handle id missing
+		}
+	}else{
+		// TODO - handle type missing
+	}
 ?>
