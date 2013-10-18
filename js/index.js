d217c6ed0ed0c276bdde07566a1983ce22172c1f
Nathaniel van Diepen <eeems@omnimaga.org>
2013-10-18 14:10:20 -0600

    jqueryify debug tools

--- js/index.js
+++ js/index.js
@@ -328,18 +328,14 @@
 	});
 	shortcut.add('f12',function(){
 		if(!flag('firebug-lite')){
-			(function(F,i,r,e,b,u,g,L,I,T,E){
-				if(F.getElementById(b))
-					return;
-				E=F[i+'NS']&&F.documentElement.namespaceURI;
-				E=E?F[i+'NS'](E,'script'):F[i]('script');
-				E[r]('id',b);
-				E[r]('src',I+g+T);
-				E[r](b,u);
-				(F[e]('head')[0]||F[e]('body')[0]).appendChild(E);
-				E=new Image;
-				E[r]('src',I+L);
-			})(document,'createElement','setAttribute','getElementsByTagName','FirebugLite','4','firebug-lite.js','releases/lite/latest/skin/xp/sprite.png','https://getfirebug.com/','#startOpened');
+			$('head').append(
+				$('<script>').attr({
+					'type': 'text/javascript',
+					'src': 'https://getfirebug.com/firebug-lite.js#startOpened',
+					'id': 'FirebugLite'
+				})
+			);
+			$('<image>').attr('src','https://getfirebug.com/releases/lite/latest/skin/xp/sprite.png');
 			flag('firebug-lite',true);
 		}
 	});
