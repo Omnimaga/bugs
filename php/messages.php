491800c08691ae8f0bc719e9777247325c8790f7
Nathaniel van Diepen <eeems@omnimaga.org>
2013-11-24 22:20:28 -0700

    Bug fix

--- php/messages.php
+++ php/messages.php
@@ -5,12 +5,12 @@
 		switch($type){
 			case 'project':
 				if($res = query("SELECT m.id, u.name, m.message, UNIX_TIMESTAMP(m.timestamp) as timestamp FROM `messages` m JOIN `users` u ON u.id = m.from_id WHERE m.p_id='%d'",Array($id))){
-					return $res->fetch_all(MYSQLI_ASSOC);
+					return $res->fetch_array(MYSQLI_ASSOC);
 				}
 			break;
 			case 'user':
 				if($res = query("SELECT  m.id, uf.name as \"from\", ut.name as \"to\", m.message, UNIX_TIMESTAMP(m.timestamp) as timestamp FROM `messages` m JOIN `users` uf ON uf.id = m.from_id JOIN `users` ut ON ut.id = m.to_id WHERE (m.from_id='%d' AND m.to_id IS NOT NULL) OR m.to_id='%d'",Array($id,$id))){
-					return $res->fetch_all(MYSQLI_ASSOC);
+					return $res->fetch_array(MYSQLI_ASSOC);
 				}
 			break;
 		}
