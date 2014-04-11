a70256aa52d8ec0f92084e9b33073ec7d405652e
Nathaniel van Diepen <eeems@omnimaga.org>
2014-04-10 18:34:56 -0600

    Bug fixes

--- php/database.php
+++ php/database.php
@@ -1,10 +1,12 @@
 <?php
 	require_once(realpath(dirname(__FILE__)).'/config.php');
 	$mysqli = new mysqli(get('host'),get('user'),get('password'),get('database'));
-	if($mysqli->connect_errno){
-		echo "Failed to connect to MySQL: ".$mysqli->connect_error;
+	if($mysqli && $mysqli->connect_errno){
+		die("Failed to connect to MySQL: ".$mysqli->connect_error);
 	}
-	$mysqli->autocommit(true);
+	if(!$mysqli->autocommit(true)){
+		die("Failed to connect to MySQL: ".$mysqli->connect_error);
+	}
 	function query($query,$args=Array()){
 		global $mysqli;
 		for ($i=0;$i<count($args);$i++){
