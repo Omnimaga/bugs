06b3822d078e0f22dc11f8a5d983d646ae7f9458
Nathaniel van Diepen <eeems@omnimaga.org>
2015-08-19 12:56:56 -0600

    Event better error display. CSS fun.

--- js/error.js
+++ js/error.js
@@ -1,19 +1,19 @@
 ready(function(){
-	dom.get('body>div')
-		.css({
-			margin: 0,
-			padding: '5px'
+	dom.get('body>div.error>div')
+		.on('click',function(){
+			dom.get(this)
+				.get('.collapsable, .collapse-arrow')
+				.each(function(){
+					if(this.classList.indexOf('collapsed') == -1){
+						this.classList.add('collapsed');
+					}else{
+						this.classList.remove('collapsed');
+					}
+				});
 		})
 		.each(function(i){
 			if(i%2 == 1){
-				dom.get(this)
-					.css({
-						backgroundColor: 'lightgray'
-					});
+				this.classList.add('odd');
 			}
-		});
-	dom.get('body>div ul')
-		.css({
-			margin: 0
 		});
 });
--- templates/error.php
+++ templates/error.php
@@ -11,34 +11,38 @@
 		<script src="js/juju/keyboard.js"></script>
 		<script src="js/juju/mouse.js"></script>
 		<script src="js/error.js"></script>
+		<link rel="stylesheet" href="css/error.css"></link>
 	</head>
 	<body>
 		<h2>
 			<?=$context->error['message']?>
 		</h2>
-		<?php
-			foreach($context->backtrace as $trace){
-				echo "<div>";
-				if(isset($trace['file'])){
-					echo "<div>Location: {$trace['file']}:{$trace['line']}</div>";
-				}
-				if(isset($trace['class'])){
-					echo "<div>Function: {$trace['class']}{$trace['type']}{$trace['function']}</div>";
-				}elseif(isset($trace['function'])){
-					echo "<div>Function: {$trace['function']}</div>";
-				}
-				if(isset($trace['args'])){
-					echo "<div>Arguments:<ul>";
-					foreach($trace['args'] as $arg){
-						echo "<li><pre>".json_encode($arg,JSON_PRETTY_PRINT)."</pre></li>";
+		<div class="error">
+			<?php
+				foreach($context->backtrace as $trace){
+					echo "<div><span class=\"collapse-arrow collapsed\">&#10097;</span>&nbsp;";
+					if(isset($trace['class'])){
+						echo "<span>Function: {$trace['class']}{$trace['type']}{$trace['function']}</span>";
+					}elseif(isset($trace['function'])){
+						echo "<span>Function: {$trace['function']}</span>";
 					}
-					echo "</ul></div>";
+					echo "<div class=\"collapsable collapsed\">";
+					if(isset($trace['file'])){
+						echo "<div>Location: {$trace['file']}:{$trace['line']}</div>";
+					}
+					if(isset($trace['args'])){
+						echo "<div>Arguments:<ul>";
+						foreach($trace['args'] as $arg){
+							echo "<li><pre>".json_encode($arg,JSON_PRETTY_PRINT)."</pre></li>";
+						}
+						echo "</ul></div>";
+					}
+					if(isset($trace['object'])){
+						echo "<div>Object:<pre>".json_encode($trace['object'],JSON_PRETTY_PRINT)."</pre></div>";
+					}
+					echo "</div></div>";
 				}
-				if(isset($trace['object'])){
-					echo "<div>Object:<pre>".json_encode($trace['object'],JSON_PRETTY_PRINT)."</pre></div>";
-				}
-				echo "</div>";
-			}
-		?>
+			?>
+		</div>
 	</body>
 </html>
