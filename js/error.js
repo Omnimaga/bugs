c81cec85819c2326590eb12540379093e89acebb
Nathaniel van Diepen <eeems@omnimaga.org>
2015-08-19 13:45:23 -0600

    Clean up error display a bit more

--- js/error.js
+++ js/error.js
@@ -1,19 +1,26 @@
 ready(function(){
-	dom.get('body>div.error>div')
-		.on('click',function(){
-			dom.get(this)
-				.get('.collapsable, .collapse-arrow')
-				.each(function(){
-					if(this.classList.indexOf('collapsed') == -1){
-						this.classList.add('collapsed');
-					}else{
-						this.classList.remove('collapsed');
-					}
-				});
-		})
+	var divs = dom.get('div.error,div.error>div.collapsable>div');
+	dom.get('div.error>div.collapsable>div')
 		.each(function(i){
 			if(i%2 == 1){
 				this.classList.add('odd');
 			}
 		});
+	divs.on('click',function(e){
+		var self = this;
+		dom.get(self)
+			.get('div.collapsable, span.collapse-arrow')
+			.each(function(){
+				if(self === this.parentNode){
+					if(this.classList.indexOf('collapsed') == -1){
+						this.classList.add('collapsed');
+					}else{
+						this.classList.remove('collapsed');
+					}
+				}
+			});
+		e.stopPropagation();
+		e.preventDefault();
+		return false;
+	});
 });
--- templates/error.php
+++ templates/error.php
@@ -1,5 +1,23 @@
 <?php
 	global $context;
+	function get_class_name($obj){
+		$name = get_class($obj);
+		if(!$name){
+			if(is_string($obj)){
+				$name = 'String';
+			}elseif(is_numeric($obj)){
+				$name = 'Number';
+			}elseif(is_array($obj)){
+				$name = 'Array';
+			}elseif(is_null($obj)){
+				$name = 'Null';
+			}else{
+				$name = 'Object';
+			}
+
+		}
+		return $name;
+	}
 ?>
 <!doctype html>
 	<head>
@@ -17,32 +35,47 @@
 		<h2>
 			<?=$context->error['message']?>
 		</h2>
+		<div>
+			<strong>
+				File:
+			</strong>
+			<?=$context->error['file']?>:<?=$context->error['line']?>
+		</div>
+		<br/>
 		<div class="error">
-			<?php
-				foreach($context->backtrace as $trace){
-					echo "<div><span class=\"collapse-arrow collapsed\">&#10097;</span>&nbsp;";
-					if(isset($trace['class'])){
-						echo "<span>Function: {$trace['class']}{$trace['type']}{$trace['function']}</span>";
-					}elseif(isset($trace['function'])){
-						echo "<span>Function: {$trace['function']}</span>";
-					}
-					echo "<div class=\"collapsable collapsed\">";
-					if(isset($trace['file'])){
-						echo "<div>Location: {$trace['file']}:{$trace['line']}</div>";
-					}
-					if(isset($trace['args'])){
-						echo "<div>Arguments:<ul>";
-						foreach($trace['args'] as $arg){
-							echo "<li><pre>".json_encode($arg,JSON_PRETTY_PRINT)."</pre></li>";
+			<span class="collapse-arrow collapsed">
+				&#10097;
+			</span>
+			<span>
+				Call Stack
+			</span>
+			<div class="collapsable collapsed">
+				<?php
+					foreach($context->backtrace as $trace){
+						echo "<div><span class=\"collapse-arrow collapsed\">&#10097;</span>&nbsp;";
+						if(isset($trace['class'])){
+							echo "<span>Function: {$trace['class']}{$trace['type']}{$trace['function']}</span>";
+						}elseif(isset($trace['function'])){
+							echo "<span>Function: {$trace['function']}</span>";
 						}
-						echo "</ul></div>";
+						echo "<div class=\"collapsable collapsed\">";
+						if(isset($trace['file'])){
+							echo "<div>Location: {$trace['file']}:{$trace['line']}</div>";
+						}
+						if(isset($trace['args'])){
+							echo "<div>Arguments:<ul>";
+							foreach($trace['args'] as $arg){
+								echo "<li><pre title=\"".get_class_name($arg)."\">".json_encode($arg,JSON_PRETTY_PRINT)."</pre></li>";
+							}
+							echo "</ul></div>";
+						}
+						if(isset($trace['object'])){
+							echo "<div>Object:<pre title=\"".get_class_name($arg)."\">".json_encode($trace['object'],JSON_PRETTY_PRINT)."</pre></div>";
+						}
+						echo "</div></div>";
 					}
-					if(isset($trace['object'])){
-						echo "<div>Object:<pre>".json_encode($trace['object'],JSON_PRETTY_PRINT)."</pre></div>";
-					}
-					echo "</div></div>";
-				}
-			?>
+				?>
+			</div>
 		</div>
 	</body>
 </html>
